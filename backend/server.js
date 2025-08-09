b36b168bb6dfddb77cea46aebf32311c2dfa1e75
vnlekcy <vnlekcy@gmail.com>
2025-08-10 01:12:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app=express();
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://assignment-1-3zh2.onrender.com", 
     methods: ["GET", "POST",],
     credentials: true
   }));
